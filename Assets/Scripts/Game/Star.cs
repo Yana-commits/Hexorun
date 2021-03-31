bf6d9a0fcf29878f84fec1a109b40073c1f1e308
yana <kovriginayana60@gmail.com>
2021-03-31 16:26:41 +0300

    explosion

--- Assets/Scripts/Game/Star.cs
+++ Assets/Scripts/Game/Star.cs
@@ -6,11 +6,17 @@ public class Star : MonoBehaviour
 {
     public delegate void CollectDelegate();
     public static event CollectDelegate Score;
+    [SerializeField]
+    private GameObject explosionPrefab;
 
+    [System.Obsolete]
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<Player>(out var player))
         {
+          var  newExplosion = Instantiate(explosionPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+            _ = newExplosion.GetComponent<ParticleSystem>().startLifetime;
+
             Handheld.Vibrate();
             Score();
             Destroy(gameObject);
